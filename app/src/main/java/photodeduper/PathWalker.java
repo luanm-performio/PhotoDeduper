dd98a75d759ab7cb891431a0dc75828d2a974ac8
Luan Mai <luan@performio.co>
2023-09-08 11:10:53 +1000

    Add PathWalker

---
+++ app/src/main/java/photodeduper/PathWalker.java
@@ -0,0 +1,41 @@
+package photodeduper;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.function.Consumer;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class PathWalker implements FileVisitor<Path> {
+
+    private final Consumer<FileMeta> fileMetaConsumer;
+    private final FileChecksum fileChecksum;
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if (Files.isRegularFile(file)) {
+            fileMetaConsumer.accept(fileChecksum.of(file));
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) {
+        return FileVisitResult.TERMINATE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+        return FileVisitResult.CONTINUE;
+    }
+}
---
+++ app/src/test/java/photodeduper/PathWalkerTest.java
@@ -0,0 +1,33 @@
+package photodeduper;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PathWalkerTest {
+
+    final List<FileMeta> allFileMetas = new ArrayList<>();
+    final PathWalker pathWalker = new PathWalker(allFileMetas::add, FileChecksum.md5());
+
+    @Test
+    void shouldHandleFilesSuccessfullyWhenAFolderIsVisited() throws Exception {
+        final Path folderPath = Paths.get("src/test/resources/test_images");
+        final String[] expectedFiles = new String[] {
+                "src/test/resources/test_images/camping DELETE ME/escape.jpg",
+                "src/test/resources/test_images/trip to the sea/s-06927.jpg",
+                "src/test/resources/test_images/sea, sand, surf/coves.jpg"
+        };
+
+        Files.walkFileTree(folderPath, pathWalker);
+
+        assertThat(allFileMetas).hasSize(3)
+                .extracting(FileMeta::filePath)
+                .containsExactlyInAnyOrder(expectedFiles);
+    }
+}
